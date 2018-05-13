667f62d00edc590791b284b64fe085f15b06057b
Your Name <you@example.com>
2018-05-13 13:22:34 +0400

    փոփոխություններ

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,13 +1,7 @@
-var fs = require('fs');
-var dummyText = "Apple yep";
+var Square = require("./module");
+var mySquareObject = new Square(5);
 
 function main() {
-   fs.writeFileSync("dummytext.txt", dummyText);
-   var text = fs.readFileSync("dummytext.txt").toString();
-   console.log(dummyText == text);
-   console.log(text);
-   fs.writeFileSync("undummytext.txt",
-       text.replace("Apple", "Microsoft")
-   );
+   console.log(mySquareObject.getArea());
 }
 main();
