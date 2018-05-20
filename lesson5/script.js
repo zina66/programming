01ea6887060a41c4265380338e3803eaf102fc6a
Your Name <you@example.com>
2018-05-20 12:21:12 +0400

    փոփոխություններ

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,26 +1,55 @@
 function main() {
-   var socket = io.connect('http://localhost:8080');
-   var chatDiv = document.getElementById('chat');
-   var input = document.getElementById('message');
-   var button = document.getElementById('submit');
-   var button2 = document.getElementById('delete');
+    var socket = io.connect('http://localhost:3000');
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+    var del = document.getElementById('del');
 
-   function handleSubmit(evt) {
-       var val = input.value;
-       if (val != "") {
-           socket.emit("send message", val);
-       }
-   }
-   button.onclick = handleSubmit;
-   function handleMessage(msg) {
-   		var p = document.createElement('p');
-   		p.innerText = msg;
-   		chatDiv.appendChild(p);
-   		input.value = "";
-}
-   
 
-socket.on('display message', handleMessage);
- // main closing bracket
-}
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+
+    button.onclick = handleSubmit;
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+    }
+
+
+    socket.on('display message', handleMessage);
+
+
+    del.onclick = delMessage;
+
+    function delMessage() {
+        socket.emit('uzum em jnjem');
+    }
+
+
+    socket.on('dzer tagery jnjeq', deleteTags);
+
+    function deleteTags() {
+        var paragraphs = document.getElementsByTagName("p");
+
+        for (var i in paragraphs) {
+            paragraphs[0].remove();
+            if(paragraphs.length == 0){
+                break;
+            }
+
+        }
+    }
+
+
+
+
+
+} // main closing bracket
+
 window.onload = main;
--- lesson5/server.js
+++ lesson5/server.js
@@ -6,16 +6,22 @@ var messages = [];
 
 app.use(express.static("."));
 app.get('/', function (req, res) {
-   res.redirect('index.html');
+    res.redirect('index.html');
 });
-server.listen(8080);
+server.listen(3000);
 
 io.on('connection', function (socket) {
-   for(var i in messages) {
-     io.sockets.emit("display message", messages[i]);
-   }
-   socket.on("send message", function (data) {
-       messages.push(data);
-       io.sockets.emit("display message", data);
-   })
+    for (var i in messages) {
+        io.sockets.emit("display message", messages[i]);
+    }
+    socket.on("send message", function (data) {
+        messages.push(data);
+        io.sockets.emit("display message", data);
+    })
+
+    socket.on('uzum em jnjem', function () {
+       messages = [];
+       io.sockets.emit('dzer tagery jnjeq');
+    });
+
 });
